7179198d9c3efe980c3d7caab30f17597b3edc3d
sryu1 <95025816+sryu1@users.noreply.github.com>
2022-12-31 20:19:42 +1000

    Improved error handling

--- pysettings.py
+++ pysettings.py
@@ -1,38 +1,35 @@
 import json
+from typing import Any, Dict, Optional
 
-
-def save_dictionary(settings, settings_file):
+def save_dictionary(settings: Dict, config_file: str) -> None:
     """Save a dictionary to a JSON file."""
     try:
-        with open(settings_file, "w") as f:
+        with open(config_file, "w") as f:
             json.dump(settings, f)
     except IOError as e:
         print(f"Error saving dictionary: {e}")
 
-
-def save_variables(settings_file, **kwargs):
+def save_variables(config_file: str, **kwargs: Any) -> None:
     """Save variables to a JSON file."""
     try:
-        with open(settings_file, "w") as f:
+        with open(config_file, "w") as f:
             json.dump(kwargs, f)
     except IOError as e:
         print(f"Error saving variables: {e}")
 
-
-def load_dictionary(settings_file):
+def load_dictionary(config_file: str) -> Dict:
     """Load a dictionary from a JSON file."""
     try:
-        with open(settings_file, "r") as f:
+        with open(config_file, "r") as f:
             return json.load(f)
     except IOError as e:
         print(f"Error loading dictionary: {e}")
         return {}
 
-
-def load_variables(settings_file, *, default=None):
+def load_variables(config_file: str, *, default: Optional[Any]=None) -> Any:
     """Load variables from a JSON file."""
     try:
-        with open(settings_file, "r") as f:
+        with open(config_file, "r") as f:
             settings = json.load(f)
     except IOError as e:
         print(f"Error loading variables: {e}")
