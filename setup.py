1d609dcd0fa9e832837eb93c5675cc51687cfa2a
sryu1 <95025816+sryu1@users.noreply.github.com>
2023-01-04 11:33:39 +1000

    New setup

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="pysettings",
-    version="0.1",
-    description="A package that saves and loads",
-    url="https://github.com/sryu1/pysettings",
-    author="sryu1",
-    license="",
-    install_requires=[],
-    packages=find_packages(),
-    classifiers=[],
-    python_requires=">=3.7",
-    include_package_data=True,
-    zip_safe=False,
-)
+setup()
